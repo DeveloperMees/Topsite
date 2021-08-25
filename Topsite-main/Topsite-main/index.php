b4e188e84b1fd3ad26f5746a46303122b3e3889d
DeveloperMees <56345458+DeveloperMees@users.noreply.github.com>
2021-08-25 15:51:50 +0200

    Add files via upload

---
+++ Topsite-main/Topsite-main/index.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Topsite</title>
+</head>
+<body>
+<header>
+    <img class="logo white_img" src="images/logo.svg" alt="">
+    <nav>
+        <ul class="nav_links">
+            <li><a href="#">Blogs</a></li>
+            <li class="dropdown">
+                <a class="dropbtn">Over ons</a>
+                <div class="dropdown-content">
+                    <a href="#">Ons verhaal</a>
+                    <a href="#">Ons team</a>
+                    <a href="#">Vacatures</a>
+                </div>
+            </li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <a href="#" class="cta">
+        <button>Bericht plaatsen <img class="white_img" src="images/arrow-right.svg" alt=""></button>
+    </a>
+</header>
+<div class="bg-image">
+    <div class="container">
+        <h1>Lorem ipsum dolor sit amet, consectetur.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, facere, sint. Ab, adipisci amet
+            aspernatur atque delectus deserunt ducimus impedit ipsam maiores maxime neque pariatur rerum sint,
+            tenetur ullam voluptates.</p>
+        <a href="#">
+            <button>Lees verder <img class="white_img" src="images/arrow-right.svg" alt=""></button>
+        </a>
+    </div>
+</div>
+
+<section>
+    <div class="gird-item"><img class="blog1" src="images/blog_afbeeldingen/blog-1.jpg" alt=""></div>
+    <div class="grid-item"><h3>Recente blogs</h3>
+        <div><img class="blog-right" src="images/blog_afbeeldingen/blog-2.jpg" alt="">
+            <div class="sidetext">
+                <h4>Stedentrip naar <br> Londen, wat neem je </h4>
+                <p>9 maart 2020</p>
+            </div>
+        </div>
+
+        <div><img class="blog-right" src="images/blog_afbeeldingen/blog-3.jpg" alt="">
+            <div class="sidetext">
+                <h4>Lekker lui brunchen <br> in Rotterdam?</h4>
+                <p>91 maart 2020</p>
+            </div>
+        </div>
+
+        <div><img class="blog-right" src="images/blog_afbeeldingen/blog-4.jpg" alt="">
+            <div class="sidetext">
+                <h4>Surflessen volgen in <br>het buitenlandd </h4>
+                <p>19 maart 2020</p>
+            </div>
+        </div>
+    </div>
+    <div class="grid-item"><h2>Tips voor een gezonde werkplek op <br> kantoor</h2>
+        <p class="section_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad alias doloribus inventore
+            laboriosam quos
+            similique, unde veritatis! Ab consectetur cupiditate dolor ipsa magnam molestiae nostrum ratione recusandae
+            sint tempore?</p>
+        <a href="#">
+            <button>Lees meer <img class="white_img" src="images/arrow-right.svg" alt=""></button>
+        </a>
+    </div>
+</section>
+
+<footer>
+    <div class="footer_bnr"></div>
+    <div class="footer_nav">
+        <img class="footer_logo white_img" src="images/logo.svg" alt="">
+        <nav>
+            <ul class="nav_footer_links">
+                <li><a href="#">Blogs</a></li>
+                <li><a href="#">Over ons</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <a class="email" href="#">info@blog.nl</a>
+    </div>
+    <div class="copyright">
+        <p>&#169 2021 - Topsite</p>
+    </div>
+</footer>
+
+</body>
+</html>
